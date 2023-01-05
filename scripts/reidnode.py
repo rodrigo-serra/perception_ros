3e0fbc770987f4e48eb9e595c90fb16a328fa8c7
Rodrigo Serra <rodrigo.serra@tecnico.ulisboa.pt>
2023-01-05 10:30:37 +0000

    Adding events functionality. Focusing on take_photo event

--- scripts/reidnode.py
+++ scripts/reidnode.py
@@ -30,6 +30,9 @@ class Reid:
         self.ctr = True
         self.detector = holisticDetector()
         self.cropOffset = 50
+        self.currentEvent = None
+        self.runHolistic = True
+        self.takePhoto = False
 
         # Create arrays of known face encodings and their names
         self.known_face_encodings = []
@@ -40,22 +43,36 @@ class Reid:
         # Subscribe to Camera Topic
         self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.imgCallback)
 
+        # Subscribe to Event and perform accordingly (start, stop, restart, automatic or non-automatic modes, take photo)
+        self.event_sub = rospy.Subscriber("/reid/event", String, self.eventCallback)
+
         # Publish Detected Faces
-        self.reid_pub = rospy.Publisher("/reid", String, queue_size=10)
+        self.reid_pub = rospy.Publisher("/reid/results", String, queue_size=10)
 
     def run(self):
         while not rospy.is_shutdown():
+            if self.currentEvent is not None:
+                rospy.logerr(str(self.currentEvent) == "take_photo")
+                if str(self.currentEvent) == "take_photo":
+                    self.takePhoto = True
+                    self.currentEvent = None
+
+
             if self.img is not None:
                 if self.ctr:
                     face_locations, face_names = faceRecognition(self.img, self.known_face_encodings, self.known_face_names)
                     
                     if face_names != []:
-                        res = self.lookIntoDetectPeople(face_locations, face_names)
+                        if self.runHolistic:
+                            res = self.lookIntoDetectPeopleHolistic(face_locations, face_names)
+                        else:
+                            res = self.lookIntoDetectPeople(face_locations, face_names)
+
                         rospy.loginfo(res)
                         self.reid_pub.publish(res)
                     else:
                         rospy.loginfo("No detections!")
-                        self.reid_pub.publish("No detections!")
+                        self.reid_pub.publish("No detections")
 
                     self.ctr = False
                     
@@ -91,10 +108,11 @@ class Reid:
         return color_image
 
 
-    def lookIntoDetectPeople(self, face_locations, face_names):
+    def lookIntoDetectPeopleHolistic(self, face_locations, face_names):
         detectionResult = ''
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            if name == "Unknown":
+            if name == "Unknown" and self.takePhoto:
+                rospy.logwarn("Taking photo to unknow person!")
                 # Draw Mask
                 mask = np.zeros(self.img.shape[:2], dtype="uint8")
                 left -= self.cropOffset
@@ -127,12 +145,48 @@ class Reid:
         
         return detectionResult
 
+
+    def lookIntoDetectPeople(self, face_locations, face_names):
+        detectionResult = ''
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            if name == "Unknown" and self.takePhoto:
+                rospy.logwarn("Taking photo to unknow person!")
+                # Draw Mask
+                mask = np.zeros(self.img.shape[:2], dtype="uint8")
+                cv2.rectangle(mask, (left, top), (right, bottom), 255, -1)
+                # cv2.imshow("Rectangular Mask", mask)
+                img_masked = cv2.bitwise_and(self.img, self.img, mask=mask)
+                # Save Img and Add to Enconder
+                imageRGB = cv2.cvtColor(img_masked, cv2.COLOR_BGR2RGB)
+                # Save new image of Person (not required)
+                im = imgPil.fromarray(imageRGB)
+                im_name = self.directory + "h" + str(self.personCounter) + ".png"
+                im.save(im_name)
+                # Load new Person to enconder
+                faceEnconder = face_recognition.face_encodings(imageRGB)
+                if len(faceEnconder) > 0:
+                    self.known_face_encodings.append(faceEnconder[0])
+                    self.known_face_names.append("H #" + str(self.personCounter))
+                    self.personCounter += 1
+            
+            
+            detectionResult += name + '; '
+        
+        return detectionResult
+
+
+
     def imgCallback(self, data):
         try:
             self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
             self.ctr = True
         except CvBridgeError as e:
             print(e)
+
+
+    def eventCallback(self, data):
+        rospy.logwarn("Got new event: " + str(data.data))
+        self.currentEvent = data.data
 
 
 
