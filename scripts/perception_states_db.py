ca7b5f25de357bbacf329d9bfe7bff6e79af022d
Rodrigo Serra <serra.rfs@gmail.com>
2023-04-10 11:39:39 +0100

    Added singleton property to semantic mapping classQ

--- scripts/perception_states_db.py
+++ scripts/perception_states_db.py
@@ -5,6 +5,13 @@ class SemanticMapping():
     '''
     description: class used to describe the semantic mapping
     '''
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
     def __init__(self):
         self.persons = []
         self.seats = []
